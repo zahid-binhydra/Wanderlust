fd87af16a86f71049915b683dc5fa4f90dc864f5
Zahid Mohammed Bin Hydra <zahidbinhydra@gmail.com>
2026-07-20 09:21:12 -0700

    Add Listing Mongoose model

---
+++ models/listing.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose")
+const Schema = mongoose.Schema
+
+const listningSchema = new Schema({
+    title: {
+       type: String,
+       require: true,
+    },
+    description: String,
+    image: {
+        filename: {
+            type: String,
+            default: "listingimage",
+        },
+        url: {
+            type: String,
+            default: "https://images.unsplash.com/photo-1782064230154-ba47c8714d9e?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+            set: (v) => v === "" ? "https://images.unsplash.com/photo-1782064230154-ba47c8714d9e?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" : v,
+        },
+    },
+    price: Number,
+    location: String,
+    country: String,
+})
+
+const Listing = mongoose.model("Listing", listningSchema)
+module.exports = Listing
